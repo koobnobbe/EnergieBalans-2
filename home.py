88373d1a4c5f76c341bd32e71c334bc2a34d1274
Koob <Koob@koobnobbe.com>
2022-10-29 12:02:16 +0200

    streamlit p1 testcase slider with df

--- home.py
+++ home.py
@@ -5,6 +5,8 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
+import plotly_express as px
+
 from st_aggrid import GridOptionsBuilder, AgGrid, GridUpdateMode, DataReturnMode
 ################
 from lib import api_p1mon as p1
@@ -21,14 +23,20 @@ st.set_page_config( page_title="EnergieBalans - Home" )
 #-------------------------------------------------------------
 # Data Collection
 #-------------------------------------------------------------
-@st.cache
-def load_p1_powergas_day():
-   data = p1.df_powergas_day
-   return data
+@st.cache(persist=True, show_spinner=True)
+def load_p1_powergas_day(reporting_year, reporting_month):
+   df = p1.df_powergas_day[['timestamp_local', 'cons', 'prd', 'afn', 'gas']]
+   df['month'] = pd.DatetimeIndex(df['timestamp_local']).month
+   df['year'] = pd.DatetimeIndex(df['timestamp_local']).year
+   df['day'] = pd.DatetimeIndex(df['timestamp_local']).day
+
+   df = df[['day','month','year', 'cons', 'prd', 'afn', 'gas']].copy()
+   df= df.query('year==' + reporting_year).query('month==' + reporting_month)
+   return df
 #-------------------------------------------------------------
 def show_table(df):
    gb = GridOptionsBuilder.from_dataframe(df)
-   gb.configure_pagination(paginationAutoPageSize=False)  # Add pagination
+   gb.configure_pagination(paginationAutoPageSize=True)  # Add pagination
    gb.configure_side_bar()  # Add a sidebar
    gb.configure_selection('multiple', use_checkbox=True,
                           groupSelectsChildren="Group checkbox select children")  # Enable multi-row selection
@@ -46,8 +54,6 @@ def show_table(df):
       #width='100%',
       reload_data=True,
    )
-   df2 = grid_response['data']
-   df2
 
 
 # -------------------------------------------------------------
@@ -59,11 +65,10 @@ with st.sidebar:
    selectedMonth = st.slider("Month :", min_value=1, max_value=12, value=const.current_month, step=1)
 #-------------------------------------------------------------
 
-df = p1.df_powergas_day[['timestamp_local','cons','prd','afn','gas']]
-df['month'] = pd.DatetimeIndex(df['timestamp_local']).month
-df['year']  = pd.DatetimeIndex(df['timestamp_local']).year
-df['day'] = pd.DatetimeIndex(df['timestamp_local']).day
-df = df.query('year==' + str(const.current_year)).query('month==' + str(const.current_month))
-df = df[['day' ,'cons','prd','afn','gas']].copy()
-
+# -------------------------------------------------------------
+# mainscreen presentation
+# -------------------------------------------------------------
+st.write(str(selectedYear)+'-'+str(selectedMonth))
+df = load_p1_powergas_day( str(selectedYear), str(selectedMonth))
 show_table(df)
+
