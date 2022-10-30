de928556bfb3894eb58a1bfa50e114fdc3d7eda4
Koob <Koob@koobnobbe.com>
2022-10-30 10:18:45 +0100

    add year pie

--- home.py
+++ home.py
@@ -1,13 +1,15 @@
 # Python App
 
 import streamlit as st
-
 import pandas as pd
 import numpy as np
 
+import plotly.graph_objs as go
 import plotly_express as px
+from plotly.subplots import make_subplots
 
 from st_aggrid import GridOptionsBuilder, AgGrid, GridUpdateMode, DataReturnMode
+
 ################
 from lib import api_p1mon as p1
 from lib import constants as const
@@ -17,7 +19,9 @@ from lib import constants as const
 #-------------------------------------------------------------
 # Application settings
 #-------------------------------------------------------------
-st.set_page_config( page_title="EnergieBalans - Home" )
+st.set_page_config( page_title="EnergieBalans - Home",layout="wide"  )
+# Use the full page instead of a narrow central column
+
 #-------------------------------------------------------------
 
 #-------------------------------------------------------------
@@ -34,7 +38,7 @@ def load_p1_powergas_day(reporting_year, reporting_month):
    df= df.query('year==' + reporting_year).query('month==' + reporting_month)
    return df
 #-------------------------------------------------------------
-def show_table(df):
+def show_table_day(df):
    gb = GridOptionsBuilder.from_dataframe(df)
    gb.configure_pagination(paginationAutoPageSize=True)  # Add pagination
    gb.configure_side_bar()  # Add a sidebar
@@ -55,6 +59,92 @@ def show_table(df):
       reload_data=True,
    )
 
+def show_graph_day(df):
+   df = df[[ 'day', 'cons', 'prd', 'afn']]
+   fig = px.histogram(df, x='day', y='cons')
+   st.plotly_chart(fig)
+
+def show_graph_vegaslite(df):
+   st.vega_lite_chart(df,
+                      {
+                         'mark': {'type': 'bar', 'tooltip': True},
+                         'encoding': {
+                            'x': {'field': 'day', 'type': 'quantitative'},
+                            'y': {'field': 'cons', 'type': 'quantitative'},
+                            #   'size': {'field': 'c', 'type': 'quantitative'},
+                            #   'color': {'field': 'c', 'type': 'quantitative'},
+                         },
+                      })
+
+def show_Graph_bar(df, title):
+   df_now = df.query('year==' + str(selectedYear))
+   df_now = df_now.groupby('month').sum().reset_index()
+
+   df_lastyear = df.query('year==' + str(selectedYear- 1))
+   df_lastyear = df_lastyear.groupby('month').sum().reset_index()
+
+   df_year2 = df.query('year==' + str(selectedYear- 2))
+   df_year2 = df_year2.groupby('month').sum().reset_index()
+
+   df_year3 = df.query('year==' + str(selectedYear - 3))
+   df_year3 = df_year3.groupby('month').sum().reset_index()
+
+   fig = make_subplots(specs=[[{"secondary_y": True}]])
+   fig.add_trace(
+      go.Bar(
+         x=df_now['month']
+         , y=df_now['cons']
+         , name=str(selectedYear)
+      )
+      , secondary_y=False
+   )
+
+   fig.add_trace(
+      go.Bar(
+         x=df_lastyear['month']
+         , y=df_lastyear['cons']
+         , name=str(selectedYear - 1)
+      )
+      , secondary_y=False
+   )
+
+   fig.add_trace(
+      go.Bar(
+         x=df_year2['month']
+         , y=df_year2['cons']
+         , name=str(selectedYear - 2)
+      )
+      , secondary_y=False
+   )
+
+   fig.add_trace(
+      go.Bar(
+         x=df_year3['month']
+         , y=df_year3['cons']
+         , name=str(selectedYear - 3)
+      )
+      , secondary_y=False
+   )
+
+   # Adding title text to the figure
+   fig.update_layout(
+      title_text='<b>'+title+'</b>'
+   )
+
+   # Naming x-axis
+   fig.update_xaxes(title_text="<b>Maand</b>")
+
+   # Naming y-axes
+   fig.update_yaxes(title_text="<b>Energie (kwh)</b>", secondary_y=False)
+   st.plotly_chart(fig)
+
+def show_Graph_circle_year(df, title):
+   df_now = df.groupby('year').sum().reset_index()
+   df = df_now[['year','cons']]
+   #df
+   fig = px.pie(df, names='year', values='cons',title=title)
+   st.plotly_chart(fig)
+
 
 # -------------------------------------------------------------
 # sidebar presentation
@@ -70,5 +160,15 @@ with st.sidebar:
 # -------------------------------------------------------------
 st.write(str(selectedYear)+'-'+str(selectedMonth))
 df = load_p1_powergas_day( str(selectedYear), str(selectedMonth))
-show_table(df)
+#show_table_day(df)
+show_graph_day(df)
+#show_graph_vegaslite(df)
+
+
+df2 = p1.df_powergas_day[['year','month','cons']]
+col1, col2 = st.columns(2)
+with col1:
+   show_Graph_bar(df2,'Stroomverbruik per jaar')
+with col2:
+   show_Graph_circle_year(df2, 'Stroomverbruik per jaar')
 
